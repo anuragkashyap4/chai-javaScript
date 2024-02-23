3705875069bd74693fc4ec34c9d632cc0e297103
anuragkashyap4 <122558507+anuragkashyap4@users.noreply.github.com>
2024-02-23 07:00:26 +0000

    data type

---
+++ 01_code.js/02_dataType.js
@@ -0,0 +1,17 @@
+"use strict" //treat all JS code as newer version. jb bhi ye lkho gy to latest javascript ka code likho gy.. jo abhi lounch hua hai..
+
+// alert(3+3) we are using nodeJs, not browser. ye browers pr run ho jaye ge console mai lakin aabhi nodeJs mai nhi hogi..
+
+// Data type
+// number => 2 to power 53
+// bigint => bhhut bade vale 
+// string => ""
+// boolean => true/false
+// null => standalone value 
+// undefine => mtlb varible define kr diya hai pr value add nhi kare..
+// symbol => unique
+
+// object 
+
+// typeof => console.log(typeof age); ye type bata ta hai..
+// null ka typeof object hota hai.
