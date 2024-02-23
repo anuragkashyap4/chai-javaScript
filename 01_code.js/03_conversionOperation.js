d0ca7f5b6e79392fba7a2fd5b927704d7addf830
anuragkashyap4 <122558507+anuragkashyap4@users.noreply.github.com>
2024-02-23 07:52:10 +0000

    conversionOperation

---
+++ 01_code.js/03_conversionOperation.js
@@ -0,0 +1,29 @@
+// let score = 33;
+
+// console.log(typeof score)
+// console.log(typeof (score))
+
+// aswer number he mile ga.
+
+let score = "33";
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber)
+
+// hamne isko number mai convert kr diya hai.. 
+// conversion ky liye first Letter Capital hona chaiye.. Number, String, Boolean, etc.
+
+// "33" => 33
+// "33abc" => NaN (Not a Number)
+// true => 1; false => 0
+
+let isLoggedIn = 1
+
+let booleanValue = Boolean(isLoggedIn)
+
+console.log(booleanValue)
+
+// 1 => true; 0 => false;
+// "" => false;
+// "hitesh" => true
+
