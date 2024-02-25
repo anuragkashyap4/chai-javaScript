3a2dd6684d8c860c4376c026b2e8248255299e50
anuragkashyap4 <122558507+anuragkashyap4@users.noreply.github.com>
2024-02-25 19:43:40 +0000

    iife

---
+++ 03_basic/04_iife.js
@@ -0,0 +1,21 @@
+// Immediately Invoked Function Expressions (IIFE) = mtlb hamne koi function likh or vo vsy he execute ho jaye..
+/*
+    IIFE DEFINATION =
+    jo function Immediately execute ho jaye.. , global scop ky polution sy problem hoti hai kai baar global scop ky jo bhi variable hai us ky jo bhi declaration hai.. us polution ko hatane ky liye IIFE ka use kara hai..
+    ()(); last mai (semicolon); lagana pdta hai ki is ko nhi pata kha stop krna hai..
+*/ 
+
+// (function defination)(function call);
+
+(function chai(){                   // named IIFE
+    console.log(`DB coonected`);
+})();
+
+// DB coonected
+
+( (name)=> {                        // Simple IIFE or arrow IIFE or UnNamed IIFE
+    console.log(`DB connect ${name}`);
+} )("annu");
+
+// DB connect annu
+
